eaa7ea5ada2c62ae6fb9ee8d4cf58e836499d254
Kiran Deshmukh <kirandeshmukh1337@gmail.com>
2024-10-18 14:42:39 +0530

    added exponential,power,divide feature

--- main.py
+++ main.py
@@ -71,27 +71,90 @@ def create_value(data, _children=(), _op=None, label=''):
     value.print = print
 
     def add(other):
+        other = other if isinstance(
+            other, SimpleNamespace) else create_value(other)
         out = create_value(value.data + other.data,
                            _children=(value, other), _op='+')
 
         def backward():
-            value.grad = 1 * out.grad
-            other.grad = 1 * out.grad
+            value.grad += 1 * out.grad
+            other.grad += 1 * out.grad
         out._backward = backward
         return out
     value.add = add
 
     def mul(other):
+        other = other if isinstance(
+            other, SimpleNamespace) else create_value(other)
         out = create_value(value.data * other.data,
                            _children=(value, other), _op='*')
 
         def backward():
-            value.grad = out.grad * other.data
-            other.grad = out.grad * value.data
+            value.grad += out.grad * other.data
+            other.grad += out.grad * value.data
         out._backward = backward
 
         return out
     value.mul = mul
+
+    # def rmul(other):
+    #     assert isinstance(other, (int, float)
+    #                       ), "only supporting int and float for now"
+
+    # value.rmul = rmul
+
+    def div(other):
+        other = other if isinstance(
+            other, SimpleNamespace) else create_value(other)
+        out = create_value(value.data / other.data,
+                           _children=(value, other), _op='/')
+
+        def backward():
+            value.grad += 1 / other.data * out.grad
+            other.grad += -value.data / (other.data ** 2) * out.grad
+        out._backward = backward
+
+        return out
+
+    value.div = div
+
+    def neg():
+        return value.mul(-1)
+    value.neg = neg
+
+    def sub(other):
+        other = other if isinstance(
+            other, SimpleNamespace) else create_value(other)
+
+        return value.add(other.neg())
+    value.sub = sub
+
+    def pow(other):
+        assert isinstance(other, (int, float)
+                          ), "only supporting int and float for now"
+
+        out = create_value(value.data**other,
+                           _op=f"**{other}", _children=(value,))
+
+        def _backward():
+            value.grad += other * (value.data**(other-1)) * out.grad
+        out.backward = _backward
+        value.pow = pow
+        return out
+
+    value.pow = pow
+
+    def exp():
+        x = value.data
+        out = create_value(math.exp(x), _op='expo', _children=(value,))
+
+        def backward():
+            value.grad += out.grad * out.data
+
+        out._backward = backward
+        return out
+
+    value.exp = exp
 
     def tanh():
         n = value.data
@@ -129,26 +192,9 @@ def create_value(data, _children=(), _op=None, label=''):
 # +  # ++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:++
 
 
-# +--------------+
-# | Basic Practice
-# +--------------+
-
-a = create_value(2.0, label='a')
-b = create_value(-3.0, label='b')
-c = create_value(10.0, label='c')
-d = a.mul(b)
-d.label = 'd'
-e = d.add(c)
-e.label = 'e'
-f = create_value(-2.0, label='f')
-L = e.mul(f)
-L.label = 'L'
-draw_dot(L)
-
 # +------------------------+
 # | Basic Neuron calculation
 # +------------------------+
-
 # inputs x1,x2
 x1 = create_value(2.0, label='x1')
 x2 = create_value(0.0, label='x2')
@@ -168,14 +214,21 @@ x1w1x2w2 = x1w1.add(x2w2)
 x1w1x2w2.label = 'x1*w1 + x2*w2'
 n = x1w1x2w2.add(b)
 n.label = 'n'
+
+# tanh() directly
 o = n.tanh()
 o.label = 'o'
 
 
-# +-------------------------+
-# | Automatic Backpropagation
-# +-------------------------+
+# breaking up tanh()
+e1 = n.rmul(2)
+e = e1.exp()
+numerator = e.sub(1)
+denominator = e.add(1)
+o = numerator.div(denominator)
+o.label = 'o'
+o.grad = 1
 o.autobackprop()
-
 draw_dot(o)
+
 # +  # ++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:+++#++:++#++:++
